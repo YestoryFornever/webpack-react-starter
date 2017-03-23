7c661291d9a4966e917725fe994d35633c3ec19e
YestoryFornever <guosj90@163.com>
2017-03-23 23:21:22 +0800

    html-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,22 @@
-var webpack = require('webpack');
-var path = require('path');
+const webpack = require('webpack');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-	entry:{//入口文件
-		//bundle:'./src/main.js',
-		index:'./src/index.html'
-	},
+	entry:'./src/main.js',//入口文件
 	output:{
 		path:path.resolve(__dirname,'dist'),//输出文件目录（__dirname指的是当前目录）
-		/**
-		 * [filename]
-		 * 输出文件名（这里的name对应entry里的key：bundle、index）
-		 * 由于index.html文件里有引入了main.js，这里把bundle行注释掉同样会生成一个index.js文件 
-		 */
-		filename:'[name].js'
+		filename:'bundle.js'
 	},
 	module:{
-		loaders:[
-			{
-				test: /\.html$/,
-				loader: "file-loader?name=[name].[ext]", 
-			}
-		]
-	}
+		loaders:[]
+	},
+	plugins:[
+		new HtmlWebpackPlugin({
+			title: 'default-title',
+			template: 'src/index.html',
+			inject: 'body',
+			hash: true
+		}),
+	],
 }
