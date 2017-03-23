4c340a4a7d2f375ee2b44303be9f93d99fde696c
YestoryFornever <guosj90@163.com>
2017-03-23 23:15:36 +0800

    file-loader
    npm i file-loader —save-dev
    见webpack.config.js文件中的注释

--- webpack.config.js
+++ webpack.config.js
@@ -2,12 +2,25 @@ var webpack = require('webpack');
 var path = require('path');
 
 module.exports = {
-	entry:'./src/main.js',//入口文件
+	entry:{//入口文件
+		//bundle:'./src/main.js',
+		index:'./src/index.html'
+	},
 	output:{
 		path:path.resolve(__dirname,'dist'),//输出文件目录（__dirname指的是当前目录）
-		filename:'bundle.js'//输出文件名
+		/**
+		 * [filename]
+		 * 输出文件名（这里的name对应entry里的key：bundle、index）
+		 * 由于index.html文件里有引入了main.js，这里把bundle行注释掉同样会生成一个index.js文件 
+		 */
+		filename:'[name].js'
 	},
 	module:{
-		loaders:[]
+		loaders:[
+			{
+				test: /\.html$/,
+				loader: "file-loader?name=[name].[ext]", 
+			}
+		]
 	}
 }
